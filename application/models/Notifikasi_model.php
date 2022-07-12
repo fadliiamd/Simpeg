c5b9236d7e6eea49109487525374c7388fc3cfd1
wajisobri <wajisobri@gmail.com>
2022-07-12 23:56:08 +0700

    Fix Bug and Added Notification

---
+++ application/models/Notifikasi_model.php
@@ -0,0 +1,66 @@
+<?php
+
+class Notifikasi_model extends CI_Model
+{
+
+    public $main_table = "notifikasi";
+    public $second_table = "account_notifikasi";
+
+    public function get_all()
+    {
+        $this->db->select('*');
+        $this->db->from($this->main_table);
+        $query = $this->db->get();  
+
+        return $query->result();
+    }
+
+    public function get_one($where)
+    {
+        return $this->db->get_where($this->main_table, $where)->row();
+    }
+
+    public function get_condition($query,$search)
+    {
+        $query = $this->db->where($query, $search)->get($this->main_table);
+
+        return $query->result();
+    }
+
+    /*
+    
+    */
+    public function get_all_where($where, $limit = 0)
+    {
+        $this->db->select('*');
+        $this->db->from($this->second_table);
+        $this->db->where($where);
+        if($limit != 0) {
+          $this->db->limit($limit);
+        }
+        $this->db->join('notifikasi', $this->main_table.'.id = '.$this->second_table.'.notifikasi_id');
+
+        return $this->db->get()->result();
+    }
+
+    public function get_all_order($coloumn, $type_order){
+        $this->db->select('*');
+        $this->db->from($this->main_table);        
+        $this->db->order_by($coloumn, $type_order);
+
+        $query = $this->db->get();        
+        
+        return $query->result();
+    }
+
+    public function create_notification($data) {
+        $this->db->insert($this->main_table, $data);
+        return ($this->db->affected_rows() != 1) ? false : $this->db->insert_id();
+    }
+
+    public function pair_notification($data) {
+        $this->db->insert($this->second_table, $data);
+        return ($this->db->affected_rows() != 1) ? false : true;
+    }
+
+}
--- application/views/partials/main-header.php
+++ application/views/partials/main-header.php
@@ -7,32 +7,32 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Skydash Admin</title>
   <!-- plugins:css -->
-  <link rel="stylesheet" href="<?= base_url(); ?>assets/vendors/feather/feather.css">
-  <link rel="stylesheet" href="<?= base_url(); ?>assets/vendors/ti-icons/css/themify-icons.css">
-  <link rel="stylesheet" href="<?= base_url(); ?>assets/vendors/css/vendor.bundle.base.css">
-  <link rel="stylesheet" href="<?= base_url(); ?>assets/vendors/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/vendors/feather/feather.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/vendors/ti-icons/css/themify-icons.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/vendors/css/vendor.bundle.base.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/vendors/mdi/css/materialdesignicons.min.css">
   <script src="https://kit.fontawesome.com/48c190b106.js" crossorigin="anonymous"></script>
   <!-- endinject -->
   <!-- Plugin css for this page -->
-  <link rel="stylesheet" href="<?= base_url(); ?>assets/vendors/datatables.net-bs4/dataTables.bootstrap4.css">
-  <link rel="stylesheet" href="<?= base_url(); ?>assets/vendors/ti-icons/css/themify-icons.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/vendors/datatables.net-bs4/dataTables.bootstrap4.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/vendors/ti-icons/css/themify-icons.css">
   <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/js/select.dataTables.min.css">
   <!-- End plugin css for this page -->
   <!-- inject:css -->
-  <link rel="stylesheet" href="<?= base_url(); ?>assets/css/vertical-layout-light/style.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/css/vertical-layout-light/style.css">
   <!-- endinject -->
-  <link rel="shortcut icon" href="<?= base_url(); ?>assets/images/favicon.png" />  
+  <link rel="shortcut icon" href="<?= base_url() ?>assets/images/favicon.png" />  
 
   <!-- plugins:js -->
-  <script src="<?= base_url(); ?>assets/vendors/js/vendor.bundle.base.js"></script>
+  <script src="<?= base_url() ?>assets/vendors/js/vendor.bundle.base.js"></script>
   <!-- endinject -->
 
   <!-- Plugin js for this page -->
-  <script src="<?= base_url(); ?>assets/vendors/chart.js/Chart.min.js"></script>
-  <script src="<?= base_url(); ?>assets/vendors/datatables.net/jquery.dataTables.js"></script>
-  <script src="<?= base_url(); ?>assets/vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script>
-  <script src="<?= base_url(); ?>assets/js/dataTables.select.min.js"></script>
-  <script src="<?= base_url(); ?>assets/js/dataTables.buttons.min.js"></script>
+  <script src="<?= base_url() ?>assets/vendors/chart.js/Chart.min.js"></script>
+  <script src="<?= base_url() ?>assets/vendors/datatables.net/jquery.dataTables.js"></script>
+  <script src="<?= base_url() ?>assets/vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script>
+  <script src="<?= base_url() ?>assets/js/dataTables.select.min.js"></script>
+  <script src="<?= base_url() ?>assets/js/dataTables.buttons.min.js"></script>
   <!-- End plugin js for this page -->
 
 </head>
@@ -41,8 +41,8 @@
     <!-- partial:partials/_navbar.html -->
     <nav class="flex-row p-0 navbar col-lg-12 col-12 fixed-top d-flex">
       <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-        <a class="mr-5 navbar-brand brand-logo" href="<?= base_url(); ?>"><img src="<?= base_url(); ?>assets/images/logo.svg" class="mr-2" alt="logo"/></a>
-        <a class="navbar-brand brand-logo-mini" href="<?= base_url(); ?>"><img src="<?= base_url(); ?>assets/images/logo-mini.svg" alt="logo"/></a>
+        <a class="mr-5 navbar-brand brand-logo" href="<?= base_url() ?>"><img src="<?= base_url() ?>assets/images/logo.svg" class="mr-2" alt="logo"/></a>
+        <a class="navbar-brand brand-logo-mini" href="<?= base_url() ?>"><img src="<?= base_url() ?>assets/images/logo-mini.svg" alt="logo"/></a>
       </div>
       <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
         <button class="navbar-toggler align-self-center" type="button" data-toggle="minimize">
@@ -66,51 +66,12 @@
               <i class="mx-0 icon-bell"></i>
               <span class="count"></span>
             </a>
-            <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
-              <p class="float-left mb-0 font-weight-normal dropdown-header">Notifications</p>
-              <a class="dropdown-item preview-item">
-                <div class="preview-thumbnail">
-                  <div class="preview-icon bg-success">
-                    <i class="mx-0 ti-info-alt"></i>
-                  </div>
-                </div>
-                <div class="preview-item-content">
-                  <h6 class="preview-subject font-weight-normal">Application Error</h6>
-                  <p class="mb-0 font-weight-light small-text text-muted">
-                    Just now
-                  </p>
-                </div>
-              </a>
-              <a class="dropdown-item preview-item">
-                <div class="preview-thumbnail">
-                  <div class="preview-icon bg-warning">
-                    <i class="mx-0 ti-settings"></i>
-                  </div>
-                </div>
-                <div class="preview-item-content">
-                  <h6 class="preview-subject font-weight-normal">Settings</h6>
-                  <p class="mb-0 font-weight-light small-text text-muted">
-                    Private message
-                  </p>
-                </div>
-              </a>
-              <a class="dropdown-item preview-item">
-                <div class="preview-thumbnail">
-                  <div class="preview-icon bg-info">
-                    <i class="mx-0 ti-user"></i>
-                  </div>
-                </div>
-                <div class="preview-item-content">
-                  <h6 class="preview-subject font-weight-normal">New user registration</h6>
-                  <p class="mb-0 font-weight-light small-text text-muted">
-                    2 days ago
-                  </p>
-                </div>
-              </a>
+            <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" id="notification_list" aria-labelledby="notificationDropdown">
+              <!-- Notification List -->
             </div>
           </li>
           <li class="nav-item nav-profile dropdown">
-            <a class="nav-link dropdown-toggle" href="<?= base_url(); ?>#" data-toggle="dropdown" id="profileDropdown">
+            <a class="nav-link dropdown-toggle" href="<?= base_url() ?>#" data-toggle="dropdown" id="profileDropdown">
               <img src="<?= base_url(); ?>assets/images/faces/face28.jpg" alt="profile"/>
             </a>
             <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
